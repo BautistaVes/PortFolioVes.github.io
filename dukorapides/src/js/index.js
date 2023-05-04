623cd87bdee84e37c84db756a50c0f4b915011c4
Bautista <devbautistaves@gmail.com>
2023-05-04 01:25:45 -0300

    duketo lindo

---
+++ dukorapides/src/js/index.js
@@ -0,0 +1,23 @@
+let primerTitulo = document.title;
+
+
+
+window.addEventListener("focus", () => {
+  setInterval(titulo, 5000);
+});
+
+function titulo() {
+  const estados = [
+    "Duko",
+    "Insano",
+    "Mis redes",
+    "𝐋𝐞𝐠𝐞𝐧𝐝𝐬",
+    primerTitulo,
+  ];
+  const random = Math.floor(Math.random() * estados.length);
+  document.title = estados[random];
+}
+
+window.addEventListener('load', function () {
+    document.querySelector('.pre-loader').className += ' hidden';
+});
