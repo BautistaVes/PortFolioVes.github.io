912177fcd63effd0318bd6fbb3455a8203b881b1
Bautista <devbautistaves@gmail.com>
2023-05-04 15:22:25 -0300

    a

--- dukorapides/src/js/index.js
+++ dukorapides/src/js/index.js
@@ -21,3 +21,21 @@ function titulo() {
 window.addEventListener('load', function () {
     document.querySelector('.pre-loader').className += ' hidden';
 });
+
+document.getElementById('main').hidden = true;
+
+var vid = document.getElementById('video');
+
+document.addEventListener('click', function () {
+	document.getElementById('enter').hidden = true;
+
+	document.getElementById('main').hidden = false;
+
+	if (typeof window.orientation == 'undefined') { 
+		vid.play();
+	}
+});
+
+vid.onended = function () {
+	vid.play();
+};
