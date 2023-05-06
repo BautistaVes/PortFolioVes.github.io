f3e712eb178d9c033c51dec6f6f13d4297897114
Bautista <devbautistaves@gmail.com>
2023-05-06 18:25:44 -0300

    a

--- dukorapides/src/js/index.js
+++ dukorapides/src/js/index.js
@@ -28,20 +28,3 @@ var vido = document.getElementById("video")
 vido.autoplay = true;
 vido.load();
 
-document.getElementById("video").play();
-
-const video = document.getElementById("video");
-video.addEventListener("play", handleFirstPlay(event), false);
-
-let hasPlayed = false;
-function handleFirstPlay(event) {
-  if (!hasPlayed) {
-    hasPlayed = true;
-
-    // Remove listener so this only gets called once.
-    const vid = event.target;
-    vid.removeEventListener("play", handleFirstPlay(event));
-
-    // Start whatever you need to do after first playback has started
-  }
-}
